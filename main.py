0944983f4a19e38f52a4580c5cc7dfc7c3d2c89e
komoshark <komoshark@gmail.com>
2024-04-04 23:59:03 -0500

    add basic files

---
+++ main.py
@@ -0,0 +1,131 @@
+from utils import evaluate_policy, Reward_adapter, str2bool
+from datetime import datetime
+from GQN import GQN_agent
+import gymnasium as gym
+import numpy as np
+import os, shutil
+import argparse
+import torch
+
+
+'''Hyperparameter Setting'''
+parser = argparse.ArgumentParser()
+parser.add_argument('--device', type=str, default='cuda', help='running device: cuda or cpu')
+parser.add_argument('--EnvIdex', type=int, default=3, help='Humanoid-v4,HalfCheetah-v4, Hopper-v4, HumanoidStandup-v4')
+parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
+parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
+parser.add_argument('--ModelIdex', type=int, default=30, help='which model to load')
+parser.add_argument('--write', type=str2bool, default=True, help='Use SummaryWriter to record the training')
+
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--update_every', type=int, default=50, help='training frequency')
+parser.add_argument('--Max_train_steps', type=int, default=int(5e6), help='Max training steps')
+parser.add_argument('--save_interval', type=int, default=int(1e5), help='Model saving interval, in steps.')
+parser.add_argument('--eval_interval', type=int, default=int(2e3), help='Model evaluating interval, in steps.')
+
+parser.add_argument('--delay_freq', type=int, default=1, help='Delayed frequency for Actor and Target Net')
+parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
+parser.add_argument('--hidden_dim', type=int, default=256, help='Hidden net width, s_dim-400-300-a_dim')
+parser.add_argument('--a_lr', type=float, default=1e-4, help='Learning rate of actor')
+parser.add_argument('--c_lr', type=float, default=1e-4, help='Learning rate of critic')
+parser.add_argument('--batch_size', type=int, default=256, help='batch_size of training')
+parser.add_argument('--explore_noise', type=float, default=0.15, help='exploring noise when interacting')
+parser.add_argument('--explore_noise_decay', type=float, default=0.998, help='Decay rate of explore noise')
+opt = parser.parse_args()
+opt.device = torch.device(opt.device) # from str to torch.device
+#print(opt)
+
+
+def main():
+    EnvName = ['Humanoid-v4','HalfCheetah-v4', 'Hopper-v4', 'HumanoidStandup-v4']
+
+    # Build Env
+    env = gym.make(EnvName[opt.EnvIdex], render_mode = "human" if opt.render else None)
+    eval_env = gym.make(EnvName[opt.EnvIdex])
+    opt.state_dim = env.observation_space.shape[0]
+    opt.action_dim = env.action_space.shape[0]
+    opt.action_bound = float(env.action_space.high[0])   #remark: action space【-max,max】
+    opt.max_e_steps = env._max_episode_steps
+    opt.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(' ---------------------------- Env ----------------------------')
+    print(f'Env:{EnvName[opt.EnvIdex]}  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
+          f'max_a:{opt.action_bound}  min_a:{env.action_space.low[0]}  max_e_steps:{opt.max_e_steps}  '
+          f'device:{opt.device}')
+    #print(env.observation_space.shape)
+    # Seed Everything
+    env_seed = opt.seed
+    np.random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed(opt.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    print("Random Seed: {}".format(opt.seed))
+
+    # Build SummaryWriter to record training curves
+    if opt.write:
+        from torch.utils.tensorboard import SummaryWriter
+        time_now = datetime.now().strftime("%d-%m-%Y %H:%M")
+        write_path = os.path.join('runs', EnvName[opt.EnvIdex] + time_now)
+        if os.path.exists(write_path):
+            shutil.rmtree(write_path)
+        writer = SummaryWriter(log_dir=write_path)
+        
+
+    # Build DRL model
+    if not os.path.exists('model'): os.mkdir('model')
+    agent = GQN_agent(**vars(opt)) # var: transfer argparse to dictionary
+    if opt.Loadmodel: agent.load(EnvName[opt.EnvIdex], opt.ModelIdex)
+
+    if opt.render:
+        while True:
+            score = evaluate_policy(env, agent, turns=1)
+            print('EnvName:', EnvName[opt.EnvIdex], 'score:', score)
+    else:
+        total_steps = 0
+        while total_steps < opt.Max_train_steps:
+            s, info = env.reset(seed=env_seed)  # Do not use opt.seed directly, or it can overfit to opt.seed
+            env_seed += 1
+            done = False
+
+            '''Interact & trian'''
+            while not done:
+                if total_steps < (10*opt.max_e_steps): 
+                    a = env.action_space.sample() # warm up
+                else:
+                    with torch.no_grad():
+                        state = torch.FloatTensor(s[np.newaxis, :]).to(opt.device)
+                        a = agent.actor(state).cpu().numpy()[0]
+                s_next, r, dw, tr, info = env.step(a) # dw: dead&win; tr: truncated
+                #r = Reward_adapter(r, opt.EnvIdex)
+                done = (dw or tr)
+
+                agent.replay_buffer.add(s, a, r, s_next, dw)
+                s = s_next
+                total_steps += 1
+
+                '''train if its time'''
+                # train 50 times every 50 steps rather than 1 training per step. Better!
+                if (total_steps >= 2*opt.max_e_steps) and (total_steps % opt.update_every == 0):
+                    for j in range(opt.update_every):
+                        agent.train()
+
+                '''record & log'''
+                if total_steps % opt.eval_interval == 0:
+                    agent.explore_noise *= opt.explore_noise_decay
+                    ep_r = evaluate_policy(eval_env, agent, opt.device, turns=3)
+                    if opt.write: writer.add_scalar('ep_r', ep_r, global_step=total_steps)
+                    print(f'EnvName:{EnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
+
+                '''save model'''
+                if total_steps % opt.save_interval == 0:
+                    agent.save(EnvName[opt.EnvIdex], int(total_steps/1000))
+        env.close()
+        eval_env.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,143 @@
+import torch.nn.functional as F
+import torch.nn as nn
+import argparse
+import torch
+import torch.distributions as dists
+import numpy as np
+from torch.distributions import Normal
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim, action_bound, gmm_k):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_mu = nn.Linear(hidden_dim, action_dim*gmm_k)
+        self.fc_lamda = nn.Linear(hidden_dim, gmm_k)
+        self.action_bound = action_bound
+        self.action_dim = action_dim
+        self.gmm_k = gmm_k
+
+    def forward(self, state):
+        x = torch.tanh(self.fc1(state))
+        x = torch.tanh(self.fc2(x))
+        lamda_array = torch.softmax(self.fc_lamda(x), dim=-1)
+        #print(lamda_array)
+        lamda_i = torch.multinomial(lamda_array, num_samples=1)
+        #lamda_i = lamda_i.squeeze(-1)
+        #indices = lamda_i.unsqueeze(1).expand(-1, self.action_dim).unsqueeze(1)
+        indices = lamda_i.expand(-1, self.action_dim).unsqueeze(1)
+        mu = torch.tanh(self.fc_mu(x))
+        mu_reshaped = mu.view(-1, self.gmm_k, self.action_dim)
+        #print(mu_reshaped)
+        mu_i = torch.gather(mu_reshaped, 1, indices).squeeze(1)
+        #print(mu_i.shape)
+        scale = torch.full(mu_i.shape, np.sqrt(0.1))  # Standard deviation is the sqrt of variance
+        dist = dists.Normal(mu_i, scale)
+        sample = dist.rsample()
+        a = torch.tanh(sample) * self.action_bound
+        #print(a.shape)
+        #mu_i = mu[lamda_i * self.action_dim : (lamda_i + 1) * self.action_dim]
+        return a
+
+class Double_Q_Critic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim):
+        super(Double_Q_Critic, self).__init__()
+
+        # Q1 architecture
+        self.fc1 = nn.Linear(state_dim + action_dim, hidden_dim) 
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_out1 = nn.Linear(hidden_dim, 1)
+
+        # Q2 architecture
+        self.fc3 = nn.Linear(state_dim + action_dim, hidden_dim)
+        self.fc4 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc_out2 = nn.Linear(hidden_dim, 1)
+
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.fc1(sa))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.fc_out1(q1)
+
+        q2 = F.relu(self.fc3(sa))
+        q2 = F.relu(self.fc4(q2))
+        q2 = self.fc_out2(q2)
+        return q1, q2
+
+
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.fc1(sa))
+        q1 = F.relu(self.fc2(q1))
+        q1 = self.fc_out1(q1)
+        return q1
+
+def evaluate_policy(env, agent, device, turns = 3):
+    total_scores = 0
+    for j in range(turns):
+        s, info = env.reset()
+        done = False
+        while not done:
+            with torch.no_grad():
+                s = torch.FloatTensor(s[np.newaxis, :]).to(device)
+                a = agent.actor(s).cpu().numpy()[0]
+                s_next, r, dw, tr, info = env.step(a)
+                done = (dw or tr)
+
+                total_scores += r
+                s = s_next
+    return int(total_scores/turns)
+
+def str2bool(v):
+    '''transfer str to bool for argparse'''
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'True','true','TRUE', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'False','false','FALSE', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+    
+#reward engineering for better training
+def Reward_adapter(r, EnvIdex):
+    # For Pendulum-v0
+    if EnvIdex == 0:
+        r = (r + 8) / 8
+
+    # For LunarLander
+    elif EnvIdex == 1:
+        if r <= -100: r = -10
+
+    # For BipedalWalker
+    elif EnvIdex == 4 or EnvIdex == 5:
+        if r <= -100: r = -1
+    return r
+
+'''
+def update_lamda(X, mu, k):
+    n, d = X.shape
+    mus = mu.reshape(k, -1)
+    
+    # Pre-allocate an array for PDF values (n samples by k distributions)
+    pdf_values = np.zeros((n, k))
+    
+    # Compute PDF values in a vectorized form
+    for i, mu in enumerate(mus):
+        # Assuming an identity covariance matrix for each distribution
+        rv = multivariate_normal(mean=mu, cov=np.eye(d)*0.1)
+        pdf_values[:, i] = rv.pdf(X) * (1e10)
+    
+    print(pdf_values)
+    # Apply softmax to PDF values to get probabilities
+    # Softmax applied across each row (axis=1)
+    #probabilities = np.exp(pdf_values - np.max(pdf_values, axis=1, keepdims=True))
+    #probabilities /= np.sum(probabilities, axis=1, keepdims=True)
+    probabilities = pdf_values.mean(axis=0)
+    probabilities /= np.sum(probabilities, axis=0, keepdims=True)
+    #mean_probabilities = probabilities.mean(axis=0)
+    return probabilities
+'''
